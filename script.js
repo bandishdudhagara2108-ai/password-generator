fd90bb6a8fc9e879515892da6b5a5ac2a8c0d95b
bandishdudhagara2108-ai <bandishdudhagara2108@gmail.com>
2026-07-05 23:16:00 +0530

    Add slider input event to update length value

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const slider = document.getElementById("length");
+const lengthValue = document.getElementById("lengthValue");
 
+slider.addEventListener("input", () => {
+    lengthValue.textContent = slider.value;
+});
